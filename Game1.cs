d04563f5a11863b7dd24c70ccc047a43dbe21fd6
Mauricio Lechuga <mlechugasanchez2857@conestogac.on.ca>
2024-11-13 20:05:14 -0500

    Initialized assets and created StartScene with menu items

--- Game1.cs
+++ Game1.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using RetroShooter.Scenes;
 
 namespace RetroShooter
 {
+    public enum GameScene
+    {
+        Start,
+        Play,
+        Help,
+        About,
+    }
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private SpriteFont _font;
+
+        private GameScene _currentScene = GameScene.Start;
+        private StartScene _startScene;
 
         public Game1()
         {
@@ -20,6 +33,7 @@ namespace RetroShooter
         {
             // TODO: Add your initialization logic here
 
+            // Set the window size
             _graphics.PreferredBackBufferWidth = 800;
             _graphics.PreferredBackBufferHeight = 1200;
             _graphics.ApplyChanges();
@@ -30,6 +44,9 @@ namespace RetroShooter
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _font = Content.Load<SpriteFont>("fonts/gameFont");
+
+            _startScene = new StartScene(_spriteBatch, _font);
 
             // TODO: use this.Content to load your game content here
         }
@@ -39,7 +56,16 @@ namespace RetroShooter
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            
+            switch (_currentScene)
+            {
+                case GameScene.Start:
+                    _startScene.Update(gameTime);
+                    break;
+
+                    // TODO: Add other scenes
+            }
+
 
             base.Update(gameTime);
         }
@@ -48,7 +74,12 @@ namespace RetroShooter
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            switch (_currentScene)
+            {
+                case GameScene.Start:
+                    _startScene.Draw(gameTime);
+                    break;
+            }
 
             base.Draw(gameTime);
         }
---
+++ Scenes/StartScene.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using RetroShooter.Scenes;
+
+namespace RetroShooter.Scenes
+{
+    public class StartScene
+    {
+        private SpriteBatch _spriteBatch;
+        private SpriteFont _font;
+        private List<string> _menuItems;
+        private int _selectedMenuItem;
+
+        public StartScene(SpriteBatch spriteBatch, SpriteFont font)
+        {
+            _spriteBatch = spriteBatch;
+            _font = font;
+            _menuItems = new List<string> { "Play", "Help", "About", "Exit" };
+            _selectedMenuItem = 0;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            var keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                _selectedMenuItem--;
+                if (_selectedMenuItem < 0)
+                {
+                    _selectedMenuItem = _menuItems.Count - 1;
+                }
+            }
+            else if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                _selectedMenuItem++;
+                if (_selectedMenuItem >= _menuItems.Count)
+                {
+                    _selectedMenuItem = 0;
+                }
+            }
+            else if (keyboardState.IsKeyDown(Keys.Enter))
+            {
+                switch (_selectedMenuItem)
+                {
+                    case 0:
+                        // TODO: Play
+                        break;
+                    case 1:
+                        // TODO: Help
+                        break;
+                    case 2:
+                        // TODO: About
+                        break;
+                    case 3:
+                        // TODO: Exit
+                        break;
+                }
+                
+            }
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            _spriteBatch.Begin();
+
+            for (int i = 0; i < _menuItems.Count; i++)
+            {
+                var color = (i == _selectedMenuItem) ? Color.Yellow : Color.White;
+                _spriteBatch.DrawString(_font, _menuItems[i], new Vector2(100, 100 + i * 50), color);
+            }
+
+            _spriteBatch.End();
+        }
+    }
+}
