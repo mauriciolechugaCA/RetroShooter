e12a492b2675f1320bb0ecdc775984743a2341e4
Mauricio Lechuga <mlechugasanchez2857@conestogac.on.ca>
2024-11-16 19:00:43 -0500

    Refactor projectiles and remove unused classes
    Removed PlayerProjectile.cs, ShieldProjectile.cs, and ShooterProjectile.cs files. Updated Projectile.cs by adding System.Numerics namespace and removing System.Threading.Tasks namespace. Expanded Projectile class to include properties for Position, Direction, Speed, and Damage, along with a constructor and methods for Update and IsOutOfBounds.

--- Entities/Projectiles/Projectile.cs
+++ Entities/Projectiles/Projectile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,27 @@ namespace RetroShooter.Entities.Projectiles
 {
     internal class Projectile
     {
+        public Vector2 Position { get; private set; }
+        public Vector2 Direction { get; private set; }
+        public float Speed { get; private set; }
+        public int Damage { get; private set; }
+
+        public Projectile(Vector2 position, Vector2 direction, float speed, int damage)
+        {
+            Position = position;
+            Direction = direction;
+            Speed = speed;
+            Damage = damage;
+        }
+
+        public void Update()
+        {
+            Position += Direction * Speed;
+        }
+
+        public bool IsOutOfBounds(Vector2 screenWidth, Vector2 screenHeight)
+        {
+            return Position.X < screenWidth.X || Position.X > screenHeight.X || Position.Y < screenWidth.Y || Position.Y > screenHeight.Y;
+        }
     }
 }
